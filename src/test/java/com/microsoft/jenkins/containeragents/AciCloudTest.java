8eb7cfd16e97567b1c97f98446f5e19e1c8d8ac1
chenyl <chenyl@microsoft.com>
2017-11-02 15:51:16 +0800

    fix endpoint issue

--- src/test/java/com/microsoft/jenkins/containeragents/AciCloudTest.java
+++ src/test/java/com/microsoft/jenkins/containeragents/AciCloudTest.java
@@ -1,21 +1,18 @@
 package com.microsoft.jenkins.containeragents;
 
-import com.microsoft.azure.management.Azure;
-import com.microsoft.jenkins.containeragents.aci.AciAgent;
-import com.microsoft.jenkins.containeragents.aci.AciService;
 
-import com.microsoft.jenkins.containeragents.util.TokenCache;
-import hudson.model.Label;
-import hudson.model.Node;
+import com.microsoft.jenkins.containeragents.util.AzureContainerUtils;
+import hudson.model.*;
+import hudson.model.labels.LabelAtom;
 import hudson.slaves.NodeProvisioner;
-import org.apache.commons.lang3.time.StopWatch;
-import org.junit.Assert;
+import hudson.tasks.Shell;
 import org.junit.ClassRule;
 import org.junit.Test;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class AciCloudTest extends IntegrationTest {
 
@@ -24,14 +21,17 @@ public class AciCloudTest extends IntegrationTest {
 
     @Test
     public void provisionAgent() throws Exception {
-
         List<NodeProvisioner.PlannedNode> r = new ArrayList<>();
         aciRule.cloud.doProvision(aciRule.template, 1, r);
 
-        Node node = r.get(0).future.get();
+        //wait to online
+        r.get(0).future.get();
 
-        Assert.assertTrue(node instanceof AciAgent);
+        FreeStyleProject project = jenkinsRule.createFreeStyleProject(AzureContainerUtils.generateName("AciTestJob", 5));
+        project.setAssignedLabel(new LabelAtom("AciTemplateTest"));
+        project.getBuildersList().add(new Shell("cd /afs"));
+        Future<FreeStyleBuild> build = project.scheduleBuild2(0);
 
-
+        jenkinsRule.assertBuildStatus(Result.SUCCESS, build.get(60, TimeUnit.SECONDS));
     }
 }
