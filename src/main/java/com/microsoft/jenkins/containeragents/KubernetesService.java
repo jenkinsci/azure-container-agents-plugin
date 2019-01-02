38173d69f21b07a140e3dc75b53c7f370021fb24
Rich Erickson <rich.erickson@fpx.com>
2019-01-02 13:12:16 -0600

    Update AzureSDK to 1.17 Update related class paths

--- src/main/java/com/microsoft/jenkins/containeragents/KubernetesService.java
+++ src/main/java/com/microsoft/jenkins/containeragents/KubernetesService.java
@@ -14,7 +14,7 @@ import com.microsoft.azure.management.resources.fluentcore.arm.ResourceUtils;
 import com.microsoft.jenkins.containeragents.helper.AzureContainerServiceCredentials;
 import com.microsoft.jenkins.containeragents.util.AzureContainerUtils;
 import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.compute.ContainerService;
+import com.microsoft.azure.management.containerservice.ContainerService;
 import com.microsoft.jenkins.azurecommons.remote.SSHClient;
 import com.microsoft.jenkins.containeragents.util.Constants;
 import hudson.security.ACL;
