a788a7c8f8d2e85bdbe379652de0ef5a9495ac29
Basil Crow <me@basilcrow.com>
2021-11-22 23:23:01 -0800

    Reduce usages of Guava (#87)

--- src/main/java/com/microsoft/jenkins/containeragents/remote/UsernamePrivateKeyAuth.java
+++ src/main/java/com/microsoft/jenkins/containeragents/remote/UsernamePrivateKeyAuth.java
@@ -5,27 +5,32 @@
 
 package com.microsoft.jenkins.containeragents.remote;
 
-import com.google.common.collect.ImmutableList;
-
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * SSH authentication credentials with username and private keys.
  */
 class UsernamePrivateKeyAuth extends UsernameAuth {
     private final String passPhrase;
-    private final ImmutableList<String> privateKeys;
+    private final List<String> privateKeys;
 
     UsernamePrivateKeyAuth(String username, String passPhrase, String... privateKeys) {
         this(username, passPhrase, Arrays.asList(privateKeys));
     }
 
-    UsernamePrivateKeyAuth(String username, String passPhrase, Iterable privateKeys) {
+    UsernamePrivateKeyAuth(String username, String passPhrase, Iterable<String> privateKeys) {
         super(username);
         this.passPhrase = passPhrase;
         //noinspection unchecked
-        this.privateKeys = ImmutableList.<String>copyOf(privateKeys);
+        List<String> privateKeyList = new ArrayList<>();
+        for (String privateKey : privateKeys) {
+            privateKeyList.add(privateKey);
+        }
+        this.privateKeys = Collections.unmodifiableList(privateKeyList);
     }
 
     byte[] getPassPhraseBytes() {
@@ -35,7 +40,7 @@ class UsernamePrivateKeyAuth extends UsernameAuth {
         return passPhrase.getBytes(StandardCharsets.UTF_8);
     }
 
-    ImmutableList<String> getPrivateKeys() {
+    List<String> getPrivateKeys() {
         return privateKeys;
     }
 }
